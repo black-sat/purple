e379f3240ae765100cdb2fb56f5b9b4678167544
Nicola Gigante <nicola.gigante@gmail.com>
2022-11-11 16:13:59 +0100

    Little fix

--- config-brew-llvm.sh
+++ config-brew-llvm.sh
@@ -2,6 +2,6 @@
 
 CC=$(brew --prefix llvm)/bin/clang \
 CXX=$(brew --prefix llvm)/bin/clang++ \
-LDFLAGS="-L$(brew --prefix llvm)/lib -Wl,-rpath,$(brew --prefix llvm)/lib" \
-CXXFLAGS="-I$(brew --prefix llvm)/include" \
+LDFLAGS="$LDFLAGS -L$(brew --prefix llvm)/lib -Wl,-rpath,$(brew --prefix llvm)/lib" \
+CXXFLAGS="$CXXFLAGS -I$(brew --prefix llvm)/include" \
 cmake "$@"
